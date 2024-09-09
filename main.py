c65edf6ef3bd07d9dbd07aefbc073c29a30b34cc
Amber Bagchi <amberbagchi2604@gmail.com>
2024-09-09 10:15:44 +0530

    prepare base model added

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from Kidney_tumor_classification import logger
 from Kidney_tumor_classification.pipeline.stage_01_data_injection import  DataIngestionTrainingPipeline
-
+from Kidney_tumor_classification.pipeline.stge_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Prepare Base Model"
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = PrepareBaseModelTrainingPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
