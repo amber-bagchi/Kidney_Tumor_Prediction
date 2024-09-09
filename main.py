c2d1b4d749e2bcc1bb2722831b27f48db3e1d276
Amber Bagchi <amberbagchi2604@gmail.com>
2024-09-09 12:31:31 +0530

    Training model added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Kidney_tumor_classification import logger
 from Kidney_tumor_classification.pipeline.stage_01_data_injection import  DataIngestionTrainingPipeline
 from Kidney_tumor_classification.pipeline.stge_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from Kidney_tumor_classification.pipeline.stage_03_training_model import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -17,6 +18,17 @@ STAGE_NAME = "Prepare Base Model"
 try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Traing Model"
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainingPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
---
+++ src/Kidney_tumor_classification/pipeline/stage_03_training_model.py
@@ -0,0 +1,28 @@
+from Kidney_tumor_classification.config.configuration import ConfigurationManager
+from Kidney_tumor_classification.components.Training_model import Training
+from Kidney_tumor_classification import logger
+
+STAGE_NAME = "Traing Model"
+
+class ModelTrainingPipeline:
+    def __init__(self) -> None:
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        training_config = config.get_training_config()
+        training = Training(config=training_config)
+        training.get_base_model()
+        training.train_valid_generator()
+        training.train()
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
